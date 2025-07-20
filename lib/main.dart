3d606d1ef5064c23d64933a0824d0f487150fb4d
oscarmwangangi <mwangangioscar2016@gmail.com>
2025-07-20 21:38:59 +0300

    july 20

---
+++ lib/main.dart
@@ -0,0 +1,117 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const Quizzler());
+
+class Quizzler extends StatelessWidget {
+  const Quizzler({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.grey.shade900,
+        body: const SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10.0),
+            child: QuizPage(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class QuizPage extends StatefulWidget {
+  const QuizPage({super.key});
+
+  @override
+  State<QuizPage> createState() => _QuizPageState();
+}
+
+class _QuizPageState extends State<QuizPage> {
+  List<Icon> scoreKeeper = [];
+
+  List<String> questions = [
+    'You can lead a cow down stairs but not up stairs.',
+    'Approximately one quarter of human bones are in the feet.',
+    'A slug\'s blood is green.',
+  ];
+
+  int questionNumber = 0;
+
+  void checkAnswer(bool userAnswer) {
+    // For now, we'll just assume all answers are true
+    bool correctAnswer = true;
+
+    setState(() {
+      if (userAnswer == correctAnswer) {
+        scoreKeeper.add(const Icon(Icons.check, color: Colors.green));
+      } else {
+        scoreKeeper.add(const Icon(Icons.close, color: Colors.red));
+      }
+
+      if (questionNumber < questions.length - 1) {
+        questionNumber++;
+      } else {
+        // Optionally, reset or show a dialog
+        questionNumber = 0;
+        scoreKeeper = [];
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Expanded(
+          flex: 5,
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Center(
+              child: Text(
+                questions[questionNumber],
+                textAlign: TextAlign.center,
+                style: const TextStyle(
+                  fontSize: 25.0,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: TextButton(
+              style: TextButton.styleFrom(backgroundColor: Colors.green),
+              child: const Text(
+                'True',
+                style: TextStyle(color: Colors.white, fontSize: 20.0),
+              ),
+              onPressed: () => checkAnswer(true),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: TextButton(
+              style: TextButton.styleFrom(backgroundColor: Colors.red),
+              child: const Text(
+                'False',
+                style: TextStyle(color: Colors.white, fontSize: 20.0),
+              ),
+              onPressed: () => checkAnswer(false),
+            ),
+          ),
+        ),
+        Row(
+          children: scoreKeeper,
+        )
+      ],
+    );
+  }
+}
